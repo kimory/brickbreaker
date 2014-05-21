1edc6da40468ab72129aa1cd1cd9b975012faa16
kimory <kimory@nuguet.fr>
2014-05-21 17:21:12 +0200

    initialisation des briques

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ brickbreaker
 ============
 
 Jeu de casse-briques avec HTML5 &amp; JavaScript
+(basé sur un tutoriel de la Ferme du Web)
--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ var ESPACE_BRIQUE = 2; // espacement entre les briques en px
 
 // Variables
 var tabBriques; // tableau virtuel contenant les briques
+var context;
 
 window.addEventListener('load', function(){
 	// On récupère l'objet Canvas
@@ -16,12 +17,46 @@ window.addEventListener('load', function(){
 	}
 
 	// On récupère le contexte 2D
-	var context = elem.getContext('2d');
+	context = elem.getContext('2d');
 
 	if(!context){
 		return;
 	}
 
 	// le navigateur est compatible + le contexte a bien été récupéré
+	// on initialise le jeu
+	creerBriques(context, NBR_LIGNES, NBR_BRIQUES_PAR_LIGNE, BRIQUE_WIDTH,
+		BRIQUE_HEIGHT, ESPACE_BRIQUE);
 
 }, false);
+
+// fonction permettant de créer les briques du jeu
+// 1 = brique existante ; 0 = brique cassée
+function creerBriques(ctx, nbrLignes, nbrParLigne, largeur, hauteur, espace) {
+
+	// Tableau virtuel: On initialise les lignes de briques
+	tabBriques = new Array(nbrLignes);
+	
+	for (var i=0; i < nbrLignes; i++) {
+		
+		// Tableau virtuel: On initialise les briques de la ligne
+		tabBriques[i] = new Array(nbrParLigne);
+		
+		// Affichage: On attribue une couleur aux briques de la ligne
+		ctx.fillStyle = "rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")";
+		
+		for (var j=0; j < nbrParLigne; j++) {
+			
+			// Affichage: On affiche une nouvelle brique
+			ctx.fillRect((j*(largeur+espace)),(i*(hauteur+espace)),largeur,hauteur);
+			
+			// Tableau virtuel: On attribue à la case actuelle la valeur 1 = la brique existe encore
+			tabBriques[i][j] = 1;
+			
+		}
+	}
+	
+	// Les briques sont initialisées.
+	return 1;
+	
+}
