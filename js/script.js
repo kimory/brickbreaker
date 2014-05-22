0851b76c753a550082c6244d71d9c6e67e82be72
kimory <kimory@nuguet.fr>
2014-05-22 18:54:18 +0200

    gestion de la raquette

--- js/script.js
+++ js/script.js
@@ -4,10 +4,23 @@ var NBR_BRIQUES_PAR_LIGNE = 8;
 var BRIQUE_WIDTH = 48; // px
 var BRIQUE_HEIGHT = 15;
 var ESPACE_BRIQUE = 2; // espacement entre les briques en px
+var BARRE_JEU_WIDTH = 80; // largeur de la barre du jeu
+var BARRE_JEU_HEIGHT = 10; // hauteur de la barre de jeu
+var PXL_DEPLA = 4;
 
 // Variables
 var tabBriques; // tableau virtuel contenant les briques
+var barreX; // position en X de la barre (changement dynamique)
+var barreY; // position en Y de la barre (ne bougera pas)
 var context;
+var zone_jeu_width;
+var zone_jeu_height;
+
+// pour les couleurs des briques, générées aléatoirement
+var couleurs_briques = [];
+for(var i = 0; i < NBR_LIGNES; i++){
+	couleurs_briques.push("rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")");
+}
 
 window.addEventListener('load', function(){
 	// On récupère l'objet Canvas
@@ -18,15 +31,29 @@ window.addEventListener('load', function(){
 
 	// On récupère le contexte 2D
 	context = elem.getContext('2d');
-
 	if(!context){
 		return;
 	}
+
+	// initialisation des variables
+	zone_jeu_width = elem.width;
+	zone_jeu_height = elem.height;
+	barreX = (zone_jeu_width/2)-(BARRE_JEU_WIDTH/2); // on calcule le centre du jeu
+	barreY= zone_jeu_height-BARRE_JEU_HEIGHT; // on calcule la position la plus basse pour la barre
 
 	// le navigateur est compatible + le contexte a bien été récupéré
 	// on initialise le jeu
 	creerBriques(context, NBR_LIGNES, NBR_BRIQUES_PAR_LIGNE, BRIQUE_WIDTH,
 		BRIQUE_HEIGHT, ESPACE_BRIQUE);
+	// on attribue la couleur de la barre + on la positionne
+	context.fillStyle = "#333333";
+	context.fillRect(barreX, barreY, BARRE_JEU_WIDTH, BARRE_JEU_HEIGHT);
+
+	// boucle de rafraichissement du contexte 2D (timer JS)
+	idInterv = setInterval(refreshGame, 10); // tous les dixièmes de seconde
+
+	// gestion des évènements
+	window.document.onkeydown = checkDepla;
 
 }, false);
 
@@ -43,7 +70,8 @@ function creerBriques(ctx, nbrLignes, nbrParLigne, largeur, hauteur, espace) {
 		tabBriques[i] = new Array(nbrParLigne);
 		
 		// Affichage: On attribue une couleur aux briques de la ligne
-		ctx.fillStyle = "rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")";
+		//ctx.fillStyle = "rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")";
+		ctx.fillStyle = couleurs_briques[i];
 		
 		for (var j=0; j < nbrParLigne; j++) {
 			
@@ -58,5 +86,49 @@ function creerBriques(ctx, nbrLignes, nbrParLigne, largeur, hauteur, espace) {
 	
 	// Les briques sont initialisées.
 	return 1;
+	
+}
+
+// fonction pour rendre transparente une zone
+function clearContexte(ctx, startwidth, ctxwidth, startheight, ctxheight) {
+	ctx.clearRect(startwidth, startheight, ctxwidth, ctxheight);
+}
+
+// contrôle de la raquette
+function checkDepla(e){
+	// Flêche de droite pressée
+	if (e.keyCode == 39){
+		// on ne déborde pas du cadre :
+		if((barreX + BARRE_JEU_WIDTH + PXL_DEPLA) <= zone_jeu_width){
+			barreX += PXL_DEPLA;
+		}
+	}
+	// Flêche de gauche pressée
+	else if (e.keyCode == 37){
+		// on ne déborde pas du cadre :
+		if((barreX - PXL_DEPLA) >= 0){
+			barreX -= PXL_DEPLA;
+		}
+	}
+}
+
+// fonction pour "mettre à jour" les briques et la barre du jeu
+function refreshGame() {
+
+	// on efface la zone
+	clearContexte(context, 0, zone_jeu_width, 0, zone_jeu_height);
+
+	// Réaffichage des briques dont l'état = 1
+	for (var i=0; i < tabBriques.length; i++) {
+		context.fillStyle = couleurs_briques[i];
+		for (var j=0; j < tabBriques[i].length; j++) {
+			if (tabBriques[i][j] == 1)
+				context.fillRect((j*(BRIQUE_WIDTH+ESPACE_BRIQUE)),(i*(BRIQUE_HEIGHT+ESPACE_BRIQUE)),BRIQUE_WIDTH,BRIQUE_HEIGHT);
+		}
+	}
+
+	// Réaffichage de la barre de jeu
+	context.fillStyle = "#333333";
+	context.fillRect(barreX,barreY,BARRE_JEU_WIDTH,BARRE_JEU_HEIGHT);
 	
 }
